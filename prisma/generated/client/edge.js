e9c301ee1ce93f22f3ac718af1a7513a81b3611b
Buddha0 <np03cs4a220303@heraldcollege.edu.np>
2025-04-13 01:05:13 -0700

    generate prisma

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -305,11 +305,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_dT0her3NOQvP@ep-shrill-truth-a5fb4dei-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
